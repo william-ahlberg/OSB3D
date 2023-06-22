4c8144d9a2e9cfaf052f728fda209551fe5d5372
Julia <julia.hannu@liu.se>
2023-06-22 15:03:02 +0200

    Recommit, procedural grid, rework prefab + prefab fix

--- OSB3D/Assets/Scripts/PlayerMove.cs
+++ OSB3D/Assets/Scripts/PlayerMove.cs
@@ -4,37 +4,42 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-	public float speed;
-	
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+
+    private KeyCode[] letterKeys;
+    private KeyCode[] arrowKeys;
+    private Vector3[] moves;
+
+    /* RIGID BODY NOT USED
+    private Rigidbody ball;
+    private float xAxis;
+    private float zAxis;*/
+
+    void Awake()
     {
-        
+        letterKeys = new KeyCode[] { KeyCode.A, KeyCode.D, KeyCode.W, KeyCode.S };
+        arrowKeys = new KeyCode[] { KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.DownArrow };
+        moves = new Vector3[] { Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        //ball = GetComponent<Rigidbody>(); 
     }
 
-    // Update is called once per frame
     void Update()
     {
-		
-		//if(Input.GetKey(KeyCode.UpArrow)
-		if(Input.GetKey(KeyCode.W))
-		{
-			transform.position += new Vector3(0,0,speed);
-		}
-		
-		if(Input.GetKey(KeyCode.S))
-		{
-			transform.position += new Vector3(0,0,-speed);
-		}
-		
-		if(Input.GetKey(KeyCode.A))
-		{
-			transform.position += new Vector3(-speed,0,0);
-		}
-		
-		if(Input.GetKey(KeyCode.D))
-		{
-			transform.position += new Vector3(speed,0,0);
-		}
+        for (int i = 0; i < letterKeys.Length; i++)
+        {
+            if (Input.GetKey(letterKeys[i]) || Input.GetKey(arrowKeys[i]))
+            {
+                transform.position += moves[i] * Time.deltaTime * speed;
+            }
+        }
+
+        /*xAxis = Input.GetAxis("Horizontal");
+        zAxis = Input.GetAxis("Vertical");*/
     }
+
+    /*void FixedUpdate()
+    {
+        ball.AddForce(new Vector3(xAxis, 0f, zAxis) * Speed);
+    }*/
 }
