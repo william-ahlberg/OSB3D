598d7dc1e1f12c59564952e27d6347128e530f6c
Julia <julia.hannu@liu.se>
2023-06-27 13:26:46 +0200

    First blocks (rough) with random placement

--- OSB3D/Assets/Scripts/LevelController.cs
+++ OSB3D/Assets/Scripts/LevelController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    public int seed; 
     public int blockCountX;
     public int blockCountZ; 
 
     public GameObject road;
     public GameObject crossing;
-    public GameObject block; 
+    //public GameObject block;
+    public GameObject[] blocks;
 
     void Start()
     {
+        //fixed numbers, the size of the 3d-assets
         float blockSize = 105;
         float roadWidth = 20; 
 
@@ -25,37 +28,48 @@ public class LevelController : MonoBehaviour
         int loopX = blockCountX * 2 - 1;
         int loopZ = blockCountZ * 2 - 1;
 
-        float currentPosX = 0;
-        float currentPosZ = 0;
-
+        //how much position should be moved (in X/Z) for every placement
         float addToPos = _blockSize / 2 + _roadWidth / 2;
 
+        //start placing at x=0, z=0 with the rotation of 0
+        float currentPosX = 0;
+        float currentPosZ = 0;
+        float yRotation = 0;
 
-        GameObject newInstance;
-        float yRotation = 0; 
+        Random.InitState(seed);
 
         for (int i = 0; i < loopX; i++)
         {
-
             for (int j = 0; j < loopZ; j++)
             {
+                GameObject newInstance;
 
+                //if both even, instatiate a block
                 if ((i % 2 == 0) && (j % 2 == 0))
                 {
-                    newInstance = Instantiate(block, new Vector3(currentPosX, 0, currentPosZ), Quaternion.identity);
+                    int chosen = Random.Range(0, blocks.Length);
+                    newInstance = Instantiate(blocks[chosen], new Vector3(currentPosX, 0, currentPosZ), Quaternion.identity);
+
+                    float rotateBlock = Random.Range(0, 4);
+                    rotateBlock *= 90;
+                    newInstance.transform.Rotate(0, rotateBlock, 0, Space.World);
                 }
 
+                //if both uneven, instantiate a crossing
                 else if ((i % 2 != 0) && (j % 2 != 0))
                 {
+
                     newInstance = Instantiate(crossing, new Vector3(currentPosX+_roadWidth/2, 0, currentPosZ+ _roadWidth / 2), Quaternion.identity);
                 }
 
+                //otherwise, instantiate a road
                 else
                 {
                     newInstance = Instantiate(road, new Vector3(currentPosX, 0, currentPosZ), Quaternion.identity);
                     newInstance.transform.Rotate(0, yRotation, 0, Space.World);
                 }
 
+                //placed all instances in the active game object
                 newInstance.transform.parent = this.transform;
 
                 currentPosZ += addToPos;
@@ -64,6 +78,7 @@ public class LevelController : MonoBehaviour
             currentPosX += addToPos;
             currentPosZ = 0;
 
+            //changes rotation every new x loop
             if (yRotation == 0) yRotation = 90;
             else yRotation = 0; 
         }
