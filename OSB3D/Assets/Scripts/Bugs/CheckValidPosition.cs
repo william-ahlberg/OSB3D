8a716fc2c8cdabeef8df3ebba4261ebc2b8df87a
William Ahlberg <wilah49@liu.se>
2023-08-25 15:44:20 +0200

    Initial version of physics bugs

---
+++ OSB3D/Assets/Scripts/Bugs/CheckValidPosition.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckValidPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPosition();
+    }
+
+    public bool CheckPosition()
+    {
+
+        RaycastHit hit;
+
+        Collider[] freeColliders = Physics.OverlapBox(transform.position, transform.localScale/2);
+        Debug.Log(freeColliders.Length);
+     
+
+        if (freeColliders.Length > 1)
+        {
+            Debug.Log("Position invalid, colliders intersect");
+            return false;
+        }
+
+        //if (Physics.Raycast(transform.position + Vector3.up * transform.localScale.y * 0.5f, Vector3.up, out hit, Mathf.Infinity))
+        //{
+        //    Debug.Log("Position invalid, roof over cube");
+        //    return false;
+        
+        //}
+
+        return true;
+    }
+}
--- OSB3D/Assets/Scripts/Bugs/GadgetBugs.cs
+++ OSB3D/Assets/Scripts/Bugs/GadgetBugs.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -42,8 +42,8 @@ public class PhysicsBugs : MonoBehaviour
 
     private void Update()
     {
-            
-    
+
+
     }
 
     private void MakeCubes(int numberOfCubes)
@@ -53,35 +53,35 @@ public class PhysicsBugs : MonoBehaviour
         for (int i = 0; i < numberOfCubes; i++)
         {
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.transform.localScale = new Vector3(Random.Range(1,5), Random.Range(1,5), Random.Range(1,5));
+            cube.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), Random.Range(1, 5));
             cube.AddComponent<CubeIntersection>();
             cube.GetComponent<Collider>().isTrigger = true;
             cube.transform.parent = parentObject.transform;
             cube.transform.position = GetRandomPosition(cube.transform.localScale, 0);
 
-            
+
         }
     }
 
     private Vector3 GetRandomPosition(Vector3 cubeScale, int depth)
     {
         RaycastHit hit;
-     
-        Bounds sceneBounds = GetBounds(); 
+
+        Bounds sceneBounds = GetBounds();
         Vector3 randomPosition = new Vector3(Random.Range(sceneBounds.min.x, sceneBounds.max.x), Random.Range(0, sceneBounds.max.y), Random.Range(sceneBounds.min.z, sceneBounds.max.z));
-        Collider[] freeColliders = Physics.OverlapBox(randomPosition, cubeScale/2);
-                      
-        if ((freeColliders.Length > 1) & (depth < 16)) 
+        Collider[] freeColliders = Physics.OverlapBox(randomPosition, cubeScale / 2);
+
+        if ((freeColliders.Length > 1) & (depth < 16))
         {
             Debug.Log("Finding new position");
             randomPosition = GetRandomPosition(cubeScale, ++depth);
         }
 
-        Physics.Raycast(randomPosition , Vector3.down, out hit, Mathf.Infinity);
-        randomPosition.y = hit.point.y + cubeScale.y/2;
-        
+        Physics.Raycast(randomPosition, Vector3.down, out hit, Mathf.Infinity);
+        randomPosition.y = hit.point.y + cubeScale.y / 2;
 
-        
+
+
         return randomPosition;
 
 
