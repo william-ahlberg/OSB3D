b87823c57b272a4906764ecd694b16106d68bfb5
William <william.s.ahlberg@gmail.com>
2024-06-25 13:37:57 +0200

    Fixing Random Monkey

--- OSB3D/Python/monkey_main.py
+++ OSB3D/Python/monkey_main.py
@@ -13,18 +13,18 @@ def main():
     parser.add_argument("-cfg", "--configuration-file", help=None)
     args = parser.parse_args()
 
-    env = OSB3DEnv(game_name=os.path.join(os.getcwd(), "../Build/osb3d"),
+    env = OSB3DEnv(game_name=os.path.join(os.getcwd(), "../Build/osb3d_random_monkey"),
                    worker_id=2,
-                   no_graphics=False,
+                   no_graphics=True,
                    seed=1,
                    max_episode_timestep=2000,
                    config_file=args.configuration_file)
     
     observation = env.reset()
     
-    agent = RandomMonkeyAgent(action_size = 6,
-                              observation_size = [1,3],
-                              is_continuous = True)
+    agent = RandomMonkeyAgent(action_size=6,
+                              observation_size=[1, 3],
+                              is_continuous=True)
     bug_cumulative = []
     for i in range(int(2000)):
         action = agent.action
@@ -51,36 +51,7 @@ def main():
     env.close()
 
     agent.save_trajectory()
-    """
-    #Create agent
-    parallel = False
-    agent = RandomMonkeyAgent(action_size = 6,
-                              observation_size = [1,3],
-                              is_continuous = True)
-    if not parallel:
-        env = OSB3DEnv(game_name=None,
-                       worker_id=0,
-                       no_graphics=False,
-                       max_episode_timestep=2000,
-                       config_file=args.configuration_file,
-                       seed=0,)
-    else:
-        envs = []
-        for i in range(3):
-            envs.append(OSB3DEnv(game_name=None,
-                       worker_id=work_id + i,
-                       no_graphics=False,
-                       max_episode_timestep=2000,
-                       config_file=args.configuration_file,))
 
-    if not parallel:
-        runner = Runner(agent=agent, frequency=frequency, env=env)
-    else:
-        runner = ParallelRunner(agent=agent, frequency=frequency, envs=envs)
-
-    runner.run()
-
-"""
 if __name__ == "__main__":
     main()
         
